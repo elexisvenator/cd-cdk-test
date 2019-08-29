f2a077925715e12a3fa8c6d6ca2d16e7c010bf62
Ben Edwards <bene@pageuppeople.com>
2019-08-30 08:19:47 +1000

    functional project

--- lib/cd-cdk-test-stack.ts
+++ lib/cd-cdk-test-stack.ts
@@ -1,9 +1,15 @@
 import cdk = require('@aws-cdk/core');
+import s3 = require('@aws-cdk/aws-s3');
 
 export class CdCdkTestStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // the code that defines your stack goes here
+    // tslint:disable-next-line:no-unused-expression
+    new s3.Bucket(this, 'MyFirstBucket', {
+      versioned: true,
+      bucketName: `pup-cdk-test-bucket-${this.node.tryGetContext('target')}`,
+    });
   }
 }
